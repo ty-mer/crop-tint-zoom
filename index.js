cdb9222be9e225b420f4ea16eaab7ea213f83852
Tyler Merle <tmerle@invsqr.com>
2021-11-07 18:48:07 -0500

    relate crop to rotation & randomize color

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var sizeOf = require('image-size');
 const { exec } = require("child_process");
-// const cropBy = 320;
-
+const cropByPercent = .09;
 async function main() {
 	const path = '/Users/tylermerle/Downloads/gws_photos/Omar Huerta - 16323225259848555278468269374968.jpg';
 	var dimensions = sizeOf(path);
 	console.log(dimensions.width, dimensions.height);
-	let cropBy = { x: dimensions.width * .09, y: dimensions.height * .09 };
-	const cmd = `~/Downloads/ffmpeg -i "${path}" -f lavfi -i "color=blue:s=${dimensions.width}x${dimensions.height}" -filter_complex "scale=${dimensions.width}:${dimensions.height},rotate=-10*PI/180,crop=${dimensions.width - (cropBy.x * 2)}:${dimensions.height - (cropBy.y * 2)}:${cropBy.x}:${cropBy.y},scale=${dimensions.width}:${dimensions.height},blend=shortest=1:all_mode=overlay:all_opacity=0.1" output.jpeg`;
+	let cropBy = { x: dimensions.width * cropByPercent, y: dimensions.height * cropByPercent };
+	let radians = 111 * cropByPercent;
+	let colors = ["yellow", "blue", "green"];
+	let color = colors[Math.floor(Math.random() * colors.length)];
+	console.log(color);
+	const cmd = `~/Downloads/ffmpeg -i "${path}" -f lavfi -i "color=${color}:s=${dimensions.width}x${dimensions.height}" -filter_complex "scale=${dimensions.width}:${dimensions.height},rotate=-${radians}*PI/180,crop=${dimensions.width - (cropBy.x * 2)}:${dimensions.height - (cropBy.y * 2)}:${cropBy.x}:${cropBy.y},scale=${dimensions.width}:${dimensions.height},blend=shortest=1:all_mode=overlay:all_opacity=0.05" output_${color}.jpeg`;
 	console.log(cmd);
 	exec(cmd, (error, stdout, stderr) => {
 		if (error) {
